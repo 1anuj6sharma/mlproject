0f2f755d65222181ed6c1d554f21b540048d1f50
Anuj Saraswat <anujpachauri441@gmail.com>
2026-04-27 05:37:44 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+# Create log file name
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+# Create logs directory ONLY
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Final log file path
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# if __name__ == "__main__":
+#     logging.info("Logging has started.")
+#     print(f"Log file created at: {LOG_FILE_PATH}")
